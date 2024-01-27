989bf97f77314615e75a1e66c808d8bb5ce297ce
pechenux <belokoniy@gmail.com>
2024-01-27 19:31:28 +0300

    add script to create out folder before executing tests

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from experiment1.experiment1 import run_experiment_1
 from experiment2.experiment2 import run_experiment_2
 
+import os
+if not os.path.exists('./out'):
+    os.makedirs('./out')
+
 # run_experiment_1()
 run_experiment_2()
