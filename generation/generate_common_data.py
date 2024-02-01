4342b73e4c43869c2dc4f040f4c6ab817735ea82
pechenux <belokoniy@gmail.com>
2024-02-01 13:26:33 +0300

    change generation sequence

--- generation/generate_common_data.py
+++ generation/generate_common_data.py
@@ -6,35 +6,33 @@ from pathlib import Path
 
 from tools.run_tests import run_tests
 
-TEST_COUNT = 10
 
-def generate_common_data(prefix, experiments, error_values, measure_function, exec_function, load_function=None):
-    print('Measuring', prefix)
-    for test_run in range(TEST_COUNT):
-        print('Run number', test_run)
-        for table in experiments["tables"]:
-            print('Running:', table['TABLE'])
-            for error_measure in ['per_value', 'per_tuple']:
-                print('Evaluating:', error_measure)
-                filename = f"out/{Path(table['TABLE']).stem}/{test_run}/{prefix}_{error_measure}.csv"
-                os.makedirs(os.path.dirname(filename), exist_ok=True)
-                try:
-                    with open(filename, 'w', newline='') as csvfile:
-                        outcsv = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
-                        outcsv.writerow(['error', 'value_exec', 'value_load'])
 
-                        for erroe_value in error_values:
-                            print('Error:', erroe_value)
-                            parameters = {
-                                "TABLE": table['TABLE'],
-                                "SEPARATOR": table['SEPARATOR'],
-                                "HAS_HEADER": table['HAS_HEADER'],
-                                "ERROR": erroe_value,
-                                "ERROR_MEASURE": error_measure,
-                            }
-                            testout = measure_function(exec_function, load_function, parameters)
-                            outcsv.writerow([erroe_value, testout[0], testout[1]])
-                except:
-                    print(f"Report {filename} did not complete, deleting incomplete data")
-                    os.remove(filename)
-                    sys.exit()
+def generate_common_data(test_run, prefix, experiments, error_values, measure_function, exec_function, load_function=None):
+    print('Run number', test_run)
+    for table in experiments["tables"]:
+        print('Running:', table['TABLE'])
+        for error_measure in ['per_value', 'per_tuple']:
+            print('Evaluating:', error_measure)
+            filename = f"out/{Path(table['TABLE']).stem}/{test_run}/{prefix}_{error_measure}.csv"
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            try:
+                with open(filename, 'w', newline='') as csvfile:
+                    outcsv = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+                    outcsv.writerow(['error', 'value_exec', 'value_load'])
+
+                    for erroe_value in error_values:
+                        print('Error:', erroe_value)
+                        parameters = {
+                            "TABLE": table['TABLE'],
+                            "SEPARATOR": table['SEPARATOR'],
+                            "HAS_HEADER": table['HAS_HEADER'],
+                            "ERROR": erroe_value,
+                            "ERROR_MEASURE": error_measure,
+                        }
+                        testout = measure_function(exec_function, load_function, parameters)
+                        outcsv.writerow([erroe_value, testout[0], testout[1]])
+            except:
+                print(f"Report {filename} did not complete, deleting incomplete data")
+                os.remove(filename)
+                sys.exit()
--- generation/generate_pfdtane_data.py
+++ generation/generate_pfdtane_data.py
@@ -16,13 +16,16 @@ ERROR_VALUES_1 = np.round(ERROR_VALUES_1, 3)
 ERROR_VALUES_2 = np.arange(0.025, 1, STEP)
 ERROR_VALUES_2 = np.round(ERROR_VALUES_2, 3)
 
+TEST_COUNT = 10
+
 def generate_pfdtane_data():
     experiments = None
     with open('generation/parameters.json') as fp:
         experiments = json.load(fp)
 
-    generate_common_data('pfdtane_time_1', experiments, ERROR_VALUES_1, measure_time, execPFDTane, loadPFDTane)
-    generate_common_data('pfdtane_memory_1', experiments, ERROR_VALUES_1, measure_memory, execfullPFDTane)
+    for test_run in range(TEST_COUNT):
+        generate_common_data(test_run, 'pfdtane_time_1', experiments, ERROR_VALUES_1, measure_time, execPFDTane, loadPFDTane)
+        generate_common_data(test_run, 'pfdtane_memory_1', experiments, ERROR_VALUES_1, measure_memory, execfullPFDTane)
 
-    generate_common_data('pfdtane_time_2', experiments, ERROR_VALUES_2, measure_time, execPFDTane, loadPFDTane)
-    # generate_common_data('pfdtane_memory_2', experiments, ERROR_VALUES_2, measure_memory, execfullPFDTane)
+        generate_common_data(test_run, 'pfdtane_time_2', experiments, ERROR_VALUES_2, measure_time, execPFDTane, loadPFDTane)
+        generate_common_data(test_run, 'pfdtane_memory_2', experiments, ERROR_VALUES_2, measure_memory, execfullPFDTane)
