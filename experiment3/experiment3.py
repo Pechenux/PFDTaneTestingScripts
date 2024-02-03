ba484c3d8a3913b351ceff3f1855879fc61b0db5
pechenux <belokoniy@gmail.com>
2024-02-03 14:50:13 +0300

    draft for experiment 3 visualisation

--- experiment3/experiment3.py
+++ experiment3/experiment3.py
@@ -2,6 +2,7 @@
 from texttable import Texttable
 import latextable
 import pandas as pd
+from functools import reduce
 
 datasets = ['BKB_WaterQualityData_2020084', 'EpicVitals', 'jena_climate_2009_2016', 'measures_v2', 'nuclear_explosions', 'parking_citations', 'SEA', 'games']
 
@@ -13,32 +14,58 @@ perfomanse_measures = {
 def get_dataframe(csv_path):
     data = pd.read_csv(csv_path, sep=' ')
     row_with_zero = data['error'] == 0
-    return data[~row_with_zero]
+    return data[~row_with_zero][['error', 'value']]
 
-def parse_csv(csv_path):
-    data = pd.read_csv(csv_path, sep=' ')
-    row_with_zero = data['error'] == 0
-    data = data[~row_with_zero]
-    return data["error"].to_numpy(), data["value"].to_numpy(), data["h"].to_numpy()
+def get_relation_between_dataframes(dataset, dataframe_pfdtane, dataframe_tane):
+    dataframe_pfdtane.drop(columns=['h'])
+    dataframe_tane.drop(columns=['h'])
+
+    dataframe_pfdtane.rename(columns={"value": "pfdvalue"})
+    dataframe_tane.rename(columns={"value": "tanevalue"})
+
+    pfdtane_values_by_tane = dataframe_pfdtane[dataframe_pfdtane['error'].isin(dataframe_tane['error'].tolist())]
+    merged_dataframe = pd.merge(pfdtane_values_by_tane, dataframe_tane, on=['error'])
+    merged_dataframe["relation"] = merged_dataframe["pfdvalue"] / merged_dataframe["tanevalue"]
+
+    return merged_dataframe[['error', f'{dataset}']]
+
+def merge_dataframes(dataframes):
+    return reduce(lambda  left, right: pd.merge(left,right,on=['error']), dataframes)
+
 
 # experiment 3a 3b table
 for perfomanse_measure in perfomanse_measures.keys():
     for error_measure in ['per_value', 'per_tuple']:
-        table_latex = Texttable()
-        table_latex.set_cols_align(['c'] * 3)
-        rows = [['Datasets',
-                 f'PFDTane {perfomanse_measures[perfomanse_measure]}',
-                 f'Tane {perfomanse_measures[perfomanse_measure]}']
-                ]
+        # rows = [['Datasets',
+        #          f'PFDTane {perfomanse_measures[perfomanse_measure]}',
+        #          f'Tane {perfomanse_measures[perfomanse_measure]}']
+        #         ]
+
+        relation_dataframes = []
 
         for dataset in datasets:
-            value_pfdtane = parse_csv(f'out/pfdtane_{perfomanse_measure}_{error_measure}_{dataset}.csv')
-            value_tane = parse_csv(f'out/tane_{perfomanse_measure}_{dataset}.csv')
-            rows.append([dataset, f"{value_pfdtane[0]} +- {value_pfdtane[1]}", f"{value_tane[0]} +- {value_tane[1]}"])
+            dataframe_pfdtane = get_dataframe(f'out/pfdtane_{perfomanse_measure}_{error_measure}_{dataset}.csv')
+            dataframe_tane = get_dataframe(f'out/tane_{perfomanse_measure}_{dataset}.csv')
+            relation_dataframes.append(get_relation_between_dataframes(dataset, dataframe_pfdtane, dataframe_tane))
+        
+        merged_dataframes = merge_dataframes(relation_dataframes)
+        error_values = merged_dataframes.iloc[[0]].to_numpy()
+        relations = merged_dataframes.iloc[1:, :]
+
+        table_latex = Texttable()
+        table_latex.set_cols_align(["c"] * (1 + len(error_values)))
+
+        rows = [['Datasets', *error_values]]
+
+        for i, row in relations.iterrows():
+            rows.append([i, *row])
         
         table_latex.add_rows(rows)
-        latex_output = latextable.draw_latex(table_latex, caption=f"{error_measure}".replace('_', '\\_'), label=f"table:{perfomanse_measure}_{error_measure}", position='ht')
 
-        with open(f'out/experiments_1_{perfomanse_measure}_{error_measure}.out', 'w') as fp:
+        multicolumn_header = [("", 1), ("error threshold", len(error_values))]
+        latex_output = latextable.draw_latex(table_latex, caption=f"Memory {error_measure}".replace('_', '\\_'), label=f"table:memory_{error_measure}", position='ht', multicolumn_header=multicolumn_header)
+
+        with open(f'out/experiments_3_{perfomanse_measure}_{error_measure}.out', 'w') as fp:
             fp.write(latex_output)
+        
 
